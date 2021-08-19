5b076fc1cac9bc22827282b9049148a90cc5a061
MargitStar <sovenokbymargo@mail.ru>
2021-08-19 15:16:23 +0300

    Add download link to song

--- music_shop/song/views.py
+++ music_shop/song/views.py
@@ -1,26 +1,31 @@
 from django.http import FileResponse
-from rest_framework import filters
+from rest_framework.response import Response
+from rest_framework import filters, viewsets, parsers, mixins
 from rest_framework.decorators import action
-from rest_framework.viewsets import ReadOnlyModelViewSet
 from django_filters.rest_framework import DjangoFilterBackend
 
-from song.models import Song
-from song.serializers import SongSerializer
+from song.models import Song, SongData
+from song.serializers import SongSerializerGet, SongDataSerializerGet
 
 
-class SongViewSet(ReadOnlyModelViewSet):
+class SongDataCreateView(viewsets.GenericViewSet, mixins.CreateModelMixin):
+    serializer_class = SongDataSerializerGet
+    queryset = SongData.objects.all()
+
+
+class SongViewSet(viewsets.ReadOnlyModelViewSet):
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     filterset_fields = ('date', 'author__name', 'author__surname', 'title', 'genre__name')
     search_fields = ['^title']
     queryset = Song.objects.all()
-    serializer_class = SongSerializer
+    serializer_class = SongSerializerGet
 
     @action(detail=True, methods=['get'])
     def data(self, request, pk=None):
-        instance = Song.objects.get(pk=pk)
+        instance = SongData.objects.get(pk=pk)
         file_handle = instance.data.open()
 
-        response = FileResponse(file_handle, content_type='audio/')
+        response = FileResponse(file_handle, content_type='audio/mpeg')
         response['Content-Length'] = instance.data.size
         response['Content-Disposition'] = 'attachment; filename="%s"' % instance.data.name
         return response
