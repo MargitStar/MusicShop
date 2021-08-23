84c9c8e257840e015a68258e954f8e2a52be8cf4
MargitStar <sovenokbymargo@mail.ru>
2021-08-23 12:39:40 +0300

    Fix urls

--- music_shop/song/urls.py
+++ music_shop/song/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
 from rest_framework import routers
+
 from song.views import SongDataCreateView, SongViewSet
 
 router = routers.SimpleRouter()
 router.register(r"songs", SongViewSet, basename="song")
 router.register(r"data", SongDataCreateView, basename="song-data")
-urlpatterns = router.urls
-urlpatterns += [
+urlpatterns = [
+    *router.urls,
     path(
         "songs/<int:pk>/playlist/<int:playlist_id>/",
         SongViewSet.as_view({"get": "playlist"}),
-    )
+    ),
 ]
