6ef6ee1f7cffef0ea2dea5e1acefcd7472ce0e9b
MargitStar <sovenokbymargo@mail.ru>
2021-08-23 11:05:00 +0300

    Change urls order

--- music_shop/music_shop/urls.py
+++ music_shop/music_shop/urls.py
@@ -19,9 +19,9 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('api/', include('song.urls')),
     path('api/', include('playlist.urls')),
-    path('admin/', admin.site.urls),
     path('api/', include('user.urls', namespace='user')),
 ]
 
