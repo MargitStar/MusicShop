50891b5439e1f6432749f2e710f2daba4aa58d28
MargitStar <sovenokbymargo@mail.ru>
2021-08-24 15:39:56 +0300

    Add list and delete (for authorized) methods

--- music_shop/tests/confest.py
+++ music_shop/tests/confest.py
@@ -1,7 +1,16 @@
 import pytest
+from django.contrib.auth import get_user_model
 from rest_framework.test import APIClient
+
+User = get_user_model()
 
 
 @pytest.fixture
 def api_client():
     return APIClient
+
+
+@pytest.mark.django_db
+def create_test_user():
+    user = User.objects.create_user(username="Star", password="star")
+    return user
--- music_shop/tests/test_playlist/e2e_tests.py
+++ music_shop/tests/test_playlist/e2e_tests.py
@@ -1,17 +1,18 @@
 import json
 
-import factory
 import pytest
 from model_bakery import baker
 
 from playlist.models import Playlist
+
+from ..confest import api_client, create_test_user
 
 pytestmark = pytest.mark.django_db
 
 
 class TestPlaylistEndpoints:
 
-    endpoint = "/api/playlist/"
+    endpoint = "/api/playlists/"
 
     def test_list(self, api_client):
         baker.make(Playlist, _quantity=3)
@@ -21,15 +22,17 @@ class TestPlaylistEndpoints:
         assert response.status_code == 200
         assert len(json.loads(response.content)) == 3
 
-    def test_create(self, api_client):
-        playlist = baker.prepare(Playlist)
-        expected_json = {
-            "name": playlist.name,
-            "user": playlist.user,
-            "song": playlist.song,
-        }
+    def test_delete(self, api_client):
+        playlist = baker.make(Playlist)
 
-        response = api_client().post(self.endpoint, data=expected_json, format="json")
+        create_test_user()
+        token_url = "/api/token/"
+        token = api_client().post(token_url, {"username": "Star", "password": "star"})
 
-        assert response.status_code == 201
-        assert json.loads(response.content) == expected_json
+        client = api_client()
+        url = f"{self.endpoint}{playlist.id}/"
+        client.credentials(HTTP_AUTHORIZATION="Bearer " + token.data["access"])
+        response = client.delete(url)
+
+        assert response.status_code == 204
+        assert Playlist.objects.all().count() == 0
