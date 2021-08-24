f409be88c4edb99c11de900a92471c608a221e51
MargitStar <sovenokbymargo@mail.ru>
2021-08-24 18:25:56 +0300

    Add test_create

--- music_shop/tests/confest.py
+++ music_shop/tests/confest.py
@@ -2,6 +2,10 @@ import pytest
 from django.contrib.auth import get_user_model
 from rest_framework.test import APIClient
 
+from author.models import Author
+from genre.models import Genre
+from song.models import SongData
+
 User = get_user_model()
 
 
@@ -14,3 +18,11 @@ def api_client():
 def create_test_user():
     user = User.objects.create_user(username="Star", password="star")
     return user
+
+
+@pytest.mark.django_db
+def create_data():
+    author = Author.objects.create(name="gleb", surname="hleb")
+    genre = Genre.objects.create(name="lal", description="pal")
+    song_data = SongData.objects.create(data="music/ed-sheeran_perfect.mp3")
+    return song_data, genre, author
--- music_shop/tests/test_song/e2e_tests.py
+++ music_shop/tests/test_song/e2e_tests.py
@@ -1,18 +1,16 @@
 import json
 
-import factory
 import pytest
 from model_bakery import baker
 
-from song.models import Song
+from song.models import Song, SongData
 
-from ..confest import api_client
+from ..confest import api_client, create_data
 
 pytestmark = pytest.mark.django_db
 
 
 class TestSongEndpoints:
-
     endpoint = "/api/songs/"
 
     def test_list(self, api_client):
@@ -22,6 +20,21 @@ class TestSongEndpoints:
 
         assert response.status_code == 200
         assert len(json.loads(response.content)) == 3
+
+    def test_create(self, api_client):
+        song = baker.make(Song)
+        song_data, genre, author = create_data()
+        expected_json = {
+            "title": song.title,
+            "author": [author.id],
+            "genre": [genre.id],
+            "release_date": str(song.release_date),
+            "data": song_data.id,
+        }
+
+        response = api_client().post(self.endpoint, data=expected_json, format="json")
+
+        assert response.status_code == 201
 
     def test_delete(self, api_client):
         song = baker.make(Song)
