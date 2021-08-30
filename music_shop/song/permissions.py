6a8f5b7367931f9c8ed4e046f6b1a55c10440095
Margarita Bobkova <56334517+MargitStar@users.noreply.github.com>
2021-08-30 13:47:44 +0300

    Update music_shop/song/permissions.py
    Co-authored-by: Vadim Denisov <q1munys@gmail.com>

--- music_shop/song/permissions.py
+++ music_shop/song/permissions.py
@@ -5,8 +5,4 @@ class ModeratorPermission(BasePermission):
     message = "Sorry, You have no permission"
 
     def has_permission(self, request, view):
-        if request.user.groups.filter(name="Moderator"):
-            return True
-
-        if request.user.is_superuser:
-            return True
+        return bool(request.user.groups.filter(name="Moderator")) or request.user.is_superuser
