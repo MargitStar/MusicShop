ae851b5a62f083419aab5eccd160ba819d38ade5
MargitStar <sovenokbymargo@mail.ru>
2021-08-30 15:35:53 +0300

    Remove if statement

--- music_shop/user/views.py
+++ music_shop/user/views.py
@@ -6,6 +6,6 @@ from user.serializers import RegisterSerializer
 class SignUpViewSet(viewsets.ViewSet):
     def create(self, request, *args, **kwargs):
         serializer = RegisterSerializer(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return response.Response(serializer.data, status=status.HTTP_201_CREATED)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return response.Response(serializer.data, status=status.HTTP_201_CREATED)
