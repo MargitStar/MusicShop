312f69a9a23e37c3f3a8d8abfa44a2d451eec9a5
MargitStar <sovenokbymargo@mail.ru>
2021-09-13 17:35:02 +0300

    Add private validation method

--- music_shop/user/managers.py
+++ music_shop/user/managers.py
@@ -2,42 +2,42 @@ from django.contrib.auth.base_user import BaseUserManager
 
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, username, password=None):
+    @staticmethod
+    def __validation(username, password=None):
         if not username:
             raise ValueError("User must have an username")
         if not password:
             raise ValueError("User must have a password")
+
+    def create_user(self, username, password=None):
+        self.__validation(username, password)
 
         user = self.model(username=username)
         user.set_password(password)
         user.is_admin = False
         user.is_staff = False
+
         user.save(using=self._db)
         return user
 
     def create_superuser(self, username, password=None):
-        if not username:
-            raise ValueError("User must have an username")
-        if not password:
-            raise ValueError("User must have a password")
+        self.__validation(username, password)
 
         user = self.model(username=username)
         user.set_password(password)
         user.is_admin = True
         user.is_staff = True
+
         user.save(using=self._db)
         return user
 
     def create_staffuser(self, username, password=None):
-        if not username:
-            raise ValueError("User must have an username")
-        if not password:
-            raise ValueError("User must have a password")
-
+        self.__validation(username, password)
         user = self.model(username=username)
-        user.set_password(password)
+
         user.set_password(password)
         user.is_admin = False
         user.is_staff = True
+
         user.save(using=self._db)
         return user
